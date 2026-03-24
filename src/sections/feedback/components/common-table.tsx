8b6ef591e561de57a7dd08264e1f4b7e32e4022b
ingenieroSenior44 <ingeniero.senior44@interrapidisimo.com>
2026-03-24 10:39:43 -0500

    feat(clients-module): configuration graph added

--- src/sections/feedback/components/common-table.tsx
+++ src/sections/feedback/components/common-table.tsx
@@ -29,6 +29,8 @@ type CommonTableProps = {
   contentTable?: any[];
   renderCell: (row: any, rowIndex: number) => React.ReactNode;
   searchable?: boolean;
+  minWidth?: number;
+
   searchPlaceholder?: string;
   filterKeys?: string[];
   filterTemplate?: React.ReactNode;
@@ -43,6 +45,7 @@ export function CommonTable({
   searchPlaceholder = 'Search...',
   filterKeys,
   filterTemplate,
+  minWidth
 }: CommonTableProps) {
   const table = useTable({ defaultOrderBy: 'orderNumber' });
   const data = useMemo(() => contentTable ?? [], [contentTable]);
@@ -118,7 +121,7 @@ export function CommonTable({
 
       <Box sx={{ position: 'relative' }}>
         <Scrollbar sx={{ minHeight: 444, overflowX: 'auto' }}>
-          <Table size={table.dense ? 'small' : 'medium'} sx={{ minWidth: 1400 }}>
+          <Table size={table.dense ? 'small' : 'medium'} sx={{ minWidth: minWidth ?? 1400 }}>
             <TableHeadCustom
               order={table.order}
               orderBy={table.orderBy}
