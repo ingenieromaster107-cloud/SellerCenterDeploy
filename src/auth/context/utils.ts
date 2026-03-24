181e99c8554a5028ee2ffa1ba340ec07119c8eaa
Miguel Angel Ortega Betancourt <ingeniero.master5@interrapidisimo.com>
2026-03-24 23:23:16 +0000

    Merged PR 35835: Agregar pagina de perfil del cliente seller
    - add page profile customer
    - structure an folder account, add route to account
    - Pantalla del perfil
    - Merge branch 'Desarrollo_Release_SellerCenter' into Desarrollo_Feature_SellerCenter_HU274236_MO
    - add configuration form account
    - Merge branch 'Desarrollo_Release_SellerCenter' into Desarrollo_Feature_SellerCenter_HU274236_MO
    - create query info customer
    - version with bug on customer
    - version beta de getcustomer
    - Merge branch 'Desarrollo_Release_SellerCenter' into Desarrollo_Feature_SellerCenter_HU274236_MO
    - pantalllas de carga y error para perfil customer

--- src/auth/context/utils.ts
+++ src/auth/context/utils.ts
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 import { jwtDecode } from 'jwt-decode';
 
 import { CUSTOMER_ID, EXPIRATION_TIME, ACCESS_TOKEN_STORAGE_KEY } from './constant';
+=======
+import type { ICustomer } from 'src/interfaces/customer/customer.interface';
+
+import { paths } from 'src/routes/paths';
+
+import { CUSTOMER_KEY, EXPIRATION_TIME, ACCESS_TOKEN_STORAGE_KEY } from './constant';
+
+>>>>>>> 23017c8 (Merged PR 35835: Agregar pagina de perfil del cliente seller)
 
 // ----------------------------------------------------------------------
 
@@ -41,6 +50,19 @@ export function setSession(accessToken: string | null) {
   }
 }
 
+
+export async function setCustomerStorage(customer: ICustomer | null) {
+  try {
+    if (customer) {
+      localStorage.setItem(CUSTOMER_KEY, JSON.stringify(customer));
+    } else {
+      localStorage.removeItem(CUSTOMER_KEY);
+    }
+  } catch (error) {
+      console.error('Error during set customer storage:', error);
+    throw error;
+  }
+}
 
 export function getSession() {
   try {
--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -3,6 +3,7 @@
 const ROOTS = {
   AUTH: '/auth',
   HOME: '/home',
+  ACCOUNT: '/account',
   PRODUCT: '/product',
   ORDER: '/order',
 };
@@ -43,6 +44,9 @@ export const paths = {
     product: {
       root: `${ROOTS.HOME}/product`,
     },
+  },
+  account:{
+    root: ROOTS.ACCOUNT,
   },
   product: {
     root: ROOTS.PRODUCT,
