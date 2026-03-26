faa925fc50b365be2b945762f79a6b17e739aff7
joseagarciap <joseagarciap@interrapidisimo.com>
2026-03-26 10:33:08 -0500

    Agregue el componente FieldsetLegend y lo integre  en AppTopProducts y ProductLoadView.

--- src/sections/overview/app/app-top-products.tsx
+++ src/sections/overview/app/app-top-products.tsx
@@ -8,6 +8,8 @@ import Card from '@mui/material/Card';
 import Avatar from '@mui/material/Avatar';
 import CardHeader from '@mui/material/CardHeader';
 
+import { FieldsetLegend } from 'src/components';
+
 // ----------------------------------------------------------------------
 
 type Props = CardProps & {
@@ -17,8 +19,10 @@ type Props = CardProps & {
 };
 
 export function AppTopProducts({ title, subheader, list, sx, ...other }: Props) {
+  console.log(other);
   return (
     <Card sx={sx} {...other}>
+      <FieldsetLegend>Top products</FieldsetLegend>
       <CardHeader title={title} subheader={subheader} />
       <Box
         sx={{
