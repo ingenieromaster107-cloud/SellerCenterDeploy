4215858623c8d3d03be0707fcb2c01734f291d01
AyrielNoriega <ingenierosenior.ecommerce@interrapidisimo.com>
2026-04-28 08:37:27 -0500

    refactor: Remove sign-up related components and tests for cleanup

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -17,23 +17,19 @@ export const paths = {
     amplify: {
       signIn: `${ROOTS.AUTH}/amplify/sign-in`,
       verify: `${ROOTS.AUTH}/amplify/verify`,
-      signUp: `${ROOTS.AUTH}/amplify/sign-up`,
       updatePassword: `${ROOTS.AUTH}/amplify/update-password`,
       resetPassword: `${ROOTS.AUTH}/amplify/reset-password`,
     },
     signIn: `${ROOTS.AUTH}/sign-in`,
-    signUp: `${ROOTS.AUTH}/sign-up`,
     firebase: {
       signIn: `${ROOTS.AUTH}/firebase/sign-in`,
       verify: `${ROOTS.AUTH}/firebase/verify`,
-      signUp: `${ROOTS.AUTH}/firebase/sign-up`,
       resetPassword: `${ROOTS.AUTH}/firebase/reset-password`,
     },
     auth0: { signIn: `${ROOTS.AUTH}/auth0/sign-in` },
     supabase: {
       signIn: `${ROOTS.AUTH}/supabase/sign-in`,
       verify: `${ROOTS.AUTH}/supabase/verify`,
-      signUp: `${ROOTS.AUTH}/supabase/sign-up`,
       updatePassword: `${ROOTS.AUTH}/supabase/update-password`,
       resetPassword: `${ROOTS.AUTH}/supabase/reset-password`,
     },
