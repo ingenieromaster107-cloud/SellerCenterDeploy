569df29fdc1acf02bab33bb4e833de9bb370e081
Sergio Eduardo Lopez Melgarejo <sergioelopezm@interrapidisimo.com>
2026-05-04 17:34:31 -0500

    fix(lint): correccion de orden en importaciones y ajustes menores

--- src/actions/return/useGetReturns.ts
+++ src/actions/return/useGetReturns.ts
@@ -3,22 +3,32 @@
 import type { ReturnListRequestInterface, ReturnListResponseInterface } from 'src/interfaces';
 
 import { useMemo } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
 
 import { GraphQLService } from 'src/lib/graphql-client';
 
 import { GET_RETURNS_QUERY } from './graphql';
 import { returnsListAdapter } from './adapters/return-list-adapter';
 
-export function useGetReturns() {
+type UseGetReturnsParams = {
+  currentPage: number;
+  pageSize: number;
+};
+
+export function useGetReturns({ currentPage, pageSize }: UseGetReturnsParams) {
 
   const graphql = GraphQLService.getInstance();
 
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['getReturns'],
-    queryFn: () => graphql.request<ReturnListResponseInterface, {}>(GET_RETURNS_QUERY),
+  const { data, isLoading, isError, isFetching } = useQuery({
+    queryKey: ['getReturns', currentPage, pageSize],
+    queryFn: () =>
+      graphql.request<ReturnListResponseInterface, { currentPage: number; pageSize: number }>(
+        GET_RETURNS_QUERY,
+        { currentPage, pageSize }
+      ),
+    placeholderData: keepPreviousData,
   });
 
   const returns = useMemo<ReturnListRequestInterface>(() => returnsListAdapter(data!), [data]);
-  return { returns, isLoading, isError };
+  return { returns, isLoading, isError, isFetching };
 }
